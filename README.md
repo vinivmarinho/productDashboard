ba58eb2eb017f08d9987274f94f9f5c56ccd2cc3
Vinícius Marinho <vini.marinho2004@gmail.com>
2026-07-07 12:04:15 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# Product Dashboard
+
+Dashboard de gerenciamento de produtos desenvolvido com **React + TypeScript**.
+
+O projeto tem como objetivo praticar conceitos do desenvolvimento Front-End moderno, como tipagem com TypeScript, criação de componentes reutilizáveis, organização de código e manipulação de dados.
+
+## 🚀 Funcionalidades planejadas
+
+- ✅ Listagem de produtos
+- ✅ Adição de novos produtos
+- ✅ Edição de produtos
+- ✅ Remoção de produtos
+- ✅ Pesquisa por nome
+- ✅ Filtro por categoria
+- ✅ Ordenação por nome ou preço
+- ✅ Interface responsiva
+
+
+
+## 🛠️ Tecnologias utilizadas
+
+- React
+- TypeScript
+- Vite
+- CSS
+- Lucide React (ícones)
+
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,31 @@
+import { Bell, Moon, Plus } from "lucide-react";
+import "./header.css"
+export default function Header() {
+    return(
+        <header className="header">
+            <div className="title">
+                <h1 className="header-title">Produtos</h1>
+                <p className="header-description">Gerencie seus produtos de forma simples e eficiente</p>
+            </div>
+    
+            <div className="icons">
+                <div className="top-icons">
+                    <Bell 
+                        size={24}
+                        strokeWidth={1.8}
+                    />
+                    <Moon 
+                        size={24}
+                        strokeWidth={1.8}
+                    />
+                </div>
+                <button className="new-product-button">
+                    <Plus size={20} /> 
+                    Novo Produto
+                </button>
+            </div>
+
+            
+        </header>
+    )
+}
