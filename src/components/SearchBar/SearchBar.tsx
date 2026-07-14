c61b5efdacacc0237766a9cf9968f82ef0223ea0
Vinícius Marinho <vini.marinho2004@gmail.com>
2026-07-14 10:15:49 -0400

    feat: Adiciona filtro de categoria, de ordenação e filtro de produtos

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,8 +1,8 @@
-import { Search } from "lucide-react";
+import { Search, ChevronDown, SlidersHorizontal } from "lucide-react";
 import "./searchBar.css";
 export default function SearchBar() {
     return(
-        <div className="filter-box">
+        <div className="toolbar">
             <div className="search-box">
                 <Search size={20} />
                 <input
@@ -12,7 +12,32 @@ export default function SearchBar() {
                 </input>
             </div>
         
-        
+        <div className="select-box">
+            <select>
+                <option>Todas as categorias</option>
+                <option>Eletrônicos</option>
+                <option>Acessórios</option>
+                <option>Móveis</option>
+            </select>
+
+            <ChevronDown size={18} />
         </div>
+
+        <div className="select-box">
+            <select>
+                <option>Ordenar por: Nome A-Z</option>
+                <option>Nome Z-A</option>
+                <option>Preço ↑</option>
+                <option>Preço ↓</option>
+            </select>
+
+            <ChevronDown size={18} />
+        </div>
+
+        <button className="filter-btn">
+            <SlidersHorizontal size={18}/>
+            Filtros
+        </button>
+        </div> /* Última div*/
     )
 }
