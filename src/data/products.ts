84d208efa661dabc86cb90b9f737f5e7866a8d4d
Vinícius Marinho <vini.marinho2004@gmail.com>
2026-07-21 10:15:18 -0400

    feat: Adiciona outras funções do CRUD

--- src/data/products.ts
+++ src/data/products.ts
@@ -9,13 +9,14 @@ export type Product = {
     stock: number;
     status: "Ativo" | "Inativo";
 }
-
-/* Array de produtos do tipo "PRODUCT" */
-export const products: Product[] = [];
+// Primeiro busca products no localStore
+const savedProducts = localStorage.getItem("products");
+// Se houver, vai para "products"
+export const products: Product[] = savedProducts ? JSON.parse(savedProducts): [];
 
 
 /* Cria um produto e adiciona ele ao array "products" */
-function createProduct(name: string, category: Product["category"], price: number, stock: number, status: Product["status"]): Product {
+export function createProduct(name: string, category: Product["category"], price: number, stock: number, status: Product["status"]): Product {
     const id = products.length + 1;
 
     const product = {
@@ -29,6 +30,49 @@ function createProduct(name: string, category: Product["category"], price: numbe
 
     products.push(product);
     console.log(`Produto ${name} criado com sucesso`);
+
+    localStorage.setItem("products", JSON.stringify(products));
     return product;
+};
+
+/* Deletar pelo id */
+export function deleteProduct(id: number) {
+
+    /* Remove produto que tenha o mesmo id do parâmetro e cria novo array */
+    const updatedProducts = products.filter(product => product.id !== id);
+    
+    products.length = 0; // Esvazia array original
+    products.push(...updatedProducts); // Adiciona ao array original o array atualizado
+
+    localStorage.setItem("products", JSON.stringify(products));
+
+    console.log(`Produto com o id ${id} removido`)
+
 }
 
+/* Atualizar o produto pelo id;
+    - updates => Objeto contento apenas os campos que serão alterados
+    - Partial<> => Torna todas as propriedades opcionais, permitindo atualizar somente os campos desejados
+    - Omit<Product, "id"> => Cria um tipo baseado em Product, mas remove a propriedade "id", impedindo que ela seja alterada
+*/
+export function updateProduct( id: number, updates: Partial<Omit<Product, "id">>): void {
+    /* Procurando o produto pelo id */
+    const product = products.find(product => product.id === id);
+
+    if (!product) {
+        console.log("Produto não encontrado");
+        return;
+    };
+
+    /* Atualiza somente os campos enviados*/
+    Object.assign(product, updates)
+
+    localStorage.setItem("products", JSON.stringify(products));
+    console.log("Produto atualizado")
+};
+
+export function ReadAllProducts():Product[] {
+    /* Pega array no localStorage e depois mostra ele em formato JSON*/
+    const savedProducts = localStorage.getItem("products");
+    return savedProducts ? JSON.parse(savedProducts) : []
+};
