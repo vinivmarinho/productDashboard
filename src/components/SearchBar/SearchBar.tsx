c4cc06b6a2c555bc1fa36a521c20b68dbae20d96
Vinícius Marinho <vini.marinho2004@gmail.com>
2026-07-29 11:26:23 -0400

    style: Adiciona responsividade para telas até 516px

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -48,38 +48,33 @@ export default function SearchBar({category, setCategory, order, setOrder, produ
                 </div>
             </div>
         
-        <div className="select-box">
-            <select
-            value={category}
-            onChange={event => setCategory(event.target.value)}
-            >
-                <option value="all">Todas as categorias</option>
-                <option value="electronics">Eletrônicos</option>
-                <option value="accessories">Acessórios</option>
-                <option value="furniture">Móveis</option>
-            </select>
+            <div className="select-box">
+                <select
+                value={category}
+                onChange={event => setCategory(event.target.value)}
+                >
+                    <option value="all">Todas as categorias</option>
+                    <option value="electronics">Eletrônicos</option>
+                    <option value="accessories">Acessórios</option>
+                    <option value="furniture">Móveis</option>
+                </select>
 
-            <ChevronDown size={18} />
-        </div>
+                <ChevronDown size={18} />
+            </div>
 
-        <div className="select-box">
-            <select
-            value={order}
-            onChange={event => setOrder(event.target.value)}
-            >
-                <option value="name-asc">Ordenar por: Nome A-Z</option>
-                <option value="name-desc">Nome Z-A</option>
-                <option value="price-asc">Menor Preço</option>
-                <option value="price-desc">Maior preço</option>
-            </select>
+            <div className="select-box">
+                <select
+                value={order}
+                onChange={event => setOrder(event.target.value)}
+                >
+                    <option value="name-asc">Ordenar por: Nome A-Z</option>
+                    <option value="name-desc">Nome Z-A</option>
+                    <option value="price-asc">Menor Preço</option>
+                    <option value="price-desc">Maior preço</option>
+                </select>
 
-            <ChevronDown size={18} />
-        </div>
-
-        <button className="filter-btn">
-            <SlidersHorizontal size={18}/>
-            Filtros
-        </button>
+                <ChevronDown size={18} />
+            </div>
         </div> 
     )
 }
--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -37,53 +37,52 @@ export default function Table({ products, setProducts, category, order } : Table
     };
 
     return(
-        <table className="products-table">
-            <thead>
-                <tr>
-                    <th>Nome</th>
-                    <th>Categoria</th>
-                    <th>Preço</th>
-                    <th>QTD. Estoque</th>
-                    <th>Status</th>
-                    <th>Ações</th>
-                </tr>
-            </thead>
-
-            <tbody>
-                
-                
-                {/* Mostrando os produtos salvos no localStorage */}
-                {sortedProducts
-                // Filtra o array products com 2 possibilidades: 
-                // Se o estado "category" for igual a "all", todos os produtos retornam true e permanecem no array
-                // Caso contrário, mantém apenas os produtos que possuem "category" igual ao valor do estado "category"
-                .filter((product) => category === "all" || product.category === category)
-                .map((product) => (
-                    
-                    <tr key={product.id}>    
-                        <td>{product.name}</td>
-                        <td>{product.category === "electronics" ? "Eletrônicos" : product.category === "accessories" ? "Acessórios" : "Móveis"}</td> {/* Converte os valores internos da categoria para os nomes exibidos na interface */}
-                        <td>R${product.price.toFixed(2)}</td>
-                        <td>{product.stock}</td>
-                        <td>
-                            <span className={`status ${product.status}`}>
-                                {product.status === "active" ? "Ativo" : "Inativo"}
-                            </span>
-                        </td>
-
-                        <td className="actions">
-                            <button>
-                                <Pencil size={18}/>
-                            </button>
-
-                            <button onClick={() => handleDelete(product.id)}>
-                                <Trash2 size={18} />
-                            </button>
-                        </td> 
+        <div className="table-container">
+            <table className="products-table">
+                <thead>
+                    <tr>
+                        <th>Nome</th>
+                        <th>Categoria</th>
+                        <th>Preço</th>
+                        <th>QTD. Estoque</th>
+                        <th>Status</th>
+                        <th>Ações</th>
                     </tr>
-                ))}
-               
-            </tbody>
-        </table>
+                </thead>
+                <tbody>
+            
+            
+                    {/* Mostrando os produtos salvos no localStorage */}
+                    {sortedProducts
+                    // Filtra o array products com 2 possibilidades:
+                    // Se o estado "category" for igual a "all", todos os produtos retornam true e permanecem no array
+                    // Caso contrário, mantém apenas os produtos que possuem "category" igual ao valor do estado "category"
+                    .filter((product) => category === "all" || product.category === category)
+                    .map((product) => (
+            
+                        <tr key={product.id}>
+                            <td>{product.name}</td>
+                            <td>{product.category === "electronics" ? "Eletrônicos" : product.category === "accessories" ? "Acessórios" : "Móveis"}</td> {/* Converte os valores internos da categoria para os nomes exibidos na interface */}
+                            <td>R${product.price.toFixed(2)}</td>
+                            <td>{product.stock}</td>
+                            <td>
+                                <span className={`status ${product.status}`}>
+                                    {product.status === "active" ? "Ativo" : "Inativo"}
+                                </span>
+                            </td>
+                            <td className="actions">
+                                <button>
+                                    <Pencil size={18}/>
+                                </button>
+                                <button onClick={() => handleDelete(product.id)}>
+                                    <Trash2 size={18} />
+                                </button>
+                            </td>
+                        </tr>
+                    ))}
+            
+                </tbody>
+            </table>
+        </div>
     )
 }
